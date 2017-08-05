6b754071ca351d65942f4971d687acdc428e88ba
su-kun1899 <higedrum.coz@gmail.com>
2017-08-05 12:27:36 +0900

    Use hello golang library

--- hello.go
+++ hello.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
+import "github.com/su-kun1899/hello-golang-library"
 
 func main() {
-  fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world. Sqrt(2) = %v\n", sample.Sqrt(2))
 }
